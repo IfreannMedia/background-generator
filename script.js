a2051330cb9e3dd1b46760e213493781305f2edd
Wario <37343942+IfreannMedia@users.noreply.github.com>
2020-10-31 15:23:04 +0100

    removed unnesecary package import

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-var _ = require('lodash');
 
 const h3 = document.getElementsByTagName("h3")[0];
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
-debugger;
-console.log(_);
+
 const eventTypesEnum = {
 	INPUT: 'input',
 	CLICK: 'click',
